4882238fa27f07960285dbb2f863ea4b2ccf8f51
pboros <pboros@tigrisdata.com>
2022-11-16 18:58:17 +0000

    have local redundant deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 REPODIR=$(dirname $0)
+if [ $# -eq 1 ]; then
+  REDUNDANT=$1
+fi
+
 cd ${REPODIR}/helm/tigris-stack
 helm dependency update
-helm upgrade --install tigris-stack . -f values-local.yaml
+if [ "x${REDUNDANT}" == "xredundant" ]; then
+  echo "Deploying a redundant cluster"
+  helm upgrade --install tigris-stack . -f values-local-redundant.yaml
+else
+  echo "Deploying non-redundant cluster"
+  helm upgrade --install tigris-stack . -f values-local.yaml
+fi
 cd -
